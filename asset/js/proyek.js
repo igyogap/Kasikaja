8d96d3c361824730f4155ec48ec81ad32d2b4bea
igyogap <78605448+igyogap@users.noreply.github.com>
2021-03-10 11:20:35 +0800

    initial commit

---
+++ asset/js/proyek.js
@@ -0,0 +1,8 @@
+let ig = document.getElementById('ig');
+ig.innerHTML = 'igyogaa';
+
+let wa = document.getElementById('wa');
+wa.innerHTML = '087861221154';
+
+let fb = document.getElementById('fb');
+fb.innerHTML = 'yoga pramana';
